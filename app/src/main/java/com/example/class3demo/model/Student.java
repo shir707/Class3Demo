97d82d5545dfdfe73acc3592b5025088e5e46c2c
DESKTOP-NFKEKDB\guylevy <guylevy2307@gmail.com>
2021-11-27 12:42:16 +0200

    adding more student details

--- app/src/main/java/com/example/class3demo/model/Student.java
+++ app/src/main/java/com/example/class3demo/model/Student.java
@@ -1,15 +1,32 @@
 package com.example.class3demo.model;
 
 public class Student {
-    String name;
-    String id;
-    boolean flag;
 
-    public  Student(){}
-    public Student(String name, String id, boolean flag) {
+    String name = "";
+    String id = "";
+    String phone="";
+    String addres="";
+    boolean flag = false;
+
+    public Student(){}
+    public Student(String name, String id, boolean flag,String phone,String addres ) {
         this.name = name;
         this.id = id;
         this.flag = flag;
+        this.phone=phone;
+        this.addres=addres;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public void setFlag(boolean flag) {
+        this.flag = flag;
+    }
+
+    public void setName(String name) {
+        this.name = name;
     }
 
     public String getName() {
@@ -24,15 +41,20 @@ public class Student {
         return flag;
     }
 
-    public void setName(String name) {
-        this.name = name;
+
+    public String getPhone() {
+        return phone;
     }
 
-    public void setId(String id) {
-        this.id = id;
+    public void setPhone(String phone) {
+        this.phone = phone;
     }
 
-    public void setFlag(boolean flag) {
-        this.flag = flag;
+    public String getAddres() {
+        return addres;
+    }
+
+    public void setAddres(String addres) {
+        this.addres = addres;
     }
 }
